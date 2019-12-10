a3339efc85aa00257eb380ab9befb3f8c19ee390
ckjeter <ckjeter7@gmail.com>
2019-12-10 21:57:11 +0800

    3_3 complete

--- hw3/prob3.cpp
+++ hw3/prob3.cpp
@@ -1,5 +1,16 @@
 /*
-2
+6
+3 0
+10 10 20
+3 1
+10 15 20
+1 6 1 10
+10 4
+3 4 5 6 7 8 9 100 22 55
+6 2 1 2
+8 99 10 1
+2 1 8 10
+10 20 9 15
 3 3
 10 10 10
 1 5 2 3
@@ -8,6 +19,10 @@
 1 1
 10
 1 6 1 3
+2 2
+10 10
+1 3 2 6
+2 7 1 2
 */
 #include <iostream>
 #include <vector>
@@ -29,7 +44,7 @@ private:
 	int num_vertex;
 	vector<vector<node> > AdjList;
 	stack<int> toposort;
-	vector<int> dp;
+	vector<long long> dp;
 public:
 	Graph():num_vertex(0){};           // default constructor
 	Graph(int N):num_vertex(N){        // constructor with input: number of vertex
@@ -39,7 +54,7 @@ public:
 	void AddEdge(int from, int to, int dist){
 		AdjList[from].push_back(node(to, dist));
 	} 
-	bool isCyclicUtil(int v, bool visited[], bool *recStack) 
+	bool isCyclicUtil(int v, vector<bool>& visited, vector<bool>& recStack) 
 	{ 
 		if(visited[v] == false) 
 		{ 
@@ -63,53 +78,33 @@ public:
 	} 
 	bool isCyclic() 
 	{ 
-		// Mark all the vertices as not visited and not part of recursion 
-		// stack 
-		bool *visited = new bool[num_vertex]; 
-		bool *recStack = new bool[num_vertex]; 
-		for(int i = 0; i < num_vertex; i++) 
-		{ 
-			visited[i] = false; 
-			recStack[i] = false; 
-		} 
-	
-		// Call the recursive helper function to detect cycle in different 
-		// DFS trees 
-		for(int i = 0; i < num_vertex; i++) 
+		vector<bool> visited(num_vertex);
+		vector<bool> recStack(num_vertex);
+		for(int i = 1; i < num_vertex; i++) 
 			if (isCyclicUtil(i, visited, recStack)) 
 				return true; 
 	
 		return false; 
 	}
 
-	void topologicalSortUtil(int v, bool visited[]) 
+	void topologicalSortUtil(int v, vector<bool>& visited) 
 	{ 
-		// Mark the current node as visited. 
-		visited[v] = true; 
-	
-		// Recur for all the vertices adjacent to this vertex 
+		visited[v] = true;  
 		vector<node>::iterator i; 
 		for (i = AdjList[v].begin(); i != AdjList[v].end(); ++i) 
 			if (!visited[i->to]) 
 				topologicalSortUtil(i->to, visited); 
-		// Push current vertex to stack which stores result 
 		toposort.push(v); 
 	} 
 	
-	// The function to do Topological Sort. It uses recursive 
-	// topologicalSortUtil() 
 	void topologicalSort() 
 	{ 
-		// Mark all the vertices as not visited 
-		bool* visited = new bool[num_vertex]; 
-		for (int i = 1; i < num_vertex; i++) 
-			visited[i] = false; 
-	
+		vector<bool> visited(num_vertex);
 		for (int i = 1; i < num_vertex; i++) 
 			if (visited[i] == false) 
 				topologicalSortUtil(i, visited);
 	} 
-	int finddiameter()
+	long long finddiameter()
 	{	
 		while (toposort.empty() == false) {  
 			int now = toposort.top();
@@ -119,7 +114,7 @@ public:
 			}
 			toposort.pop();
 		}
-		int answer = 0;
+		long long answer = 0;
 		for (int i = 1; i < dp.size(); i++){
 			answer = max(answer, dp[i]);
 		}
@@ -138,20 +133,27 @@ public:
 	}
 };
 
+int bsearch(int left, int right, int value, vector<int>& v)
+{
+	while(left < right) {
+	   int mid = left + (right - left) / 2;
+	   if (value > v[mid]) left = mid + 1;
+	   else if (value < v[mid]) right = mid - 1;
+       else return mid;
+	}
+	return (value > v[left]) ? (left + 1) : left;
+}
 int main(){
     cin.tie(0);
     ios_base::sync_with_stdio(false);
 	int testcase;
 	cin>>testcase;
-	int answer[testcase];
+	long long answer[11] = {0};
 	for (int p = 0; p < testcase; p++){
 		int N,M;
 		cin>>N>>M;
-		
-		vector<unordered_map<int, int> > nodemap; //<local index, global index>
-		nodemap.resize(N+1);
-		unordered_map<int, int>::iterator it;
-		int jumppoint[M][4];
+
+		vector<int> nodemap[N+1][2];
 		int nodecount = 0;
 
 		//維護每首歌的頭尾點
@@ -160,70 +162,82 @@ int main(){
 			int length;
 			cin>>length;
 			longestsong = max(longestsong, length);
-			nodemap[i].insert(make_pair(1, ++nodecount));
-			nodemap[i].insert(make_pair(length, ++nodecount));
+			nodemap[i][0].push_back(1);
+			nodemap[i][1].push_back(++nodecount);
+			nodemap[i][0].push_back(length);
+			nodemap[i][1].push_back(++nodecount);
 		}
+		
 		if (M == 0){
 			answer[p] = longestsong;
 			continue;
 		}
+		
+		int jumppoint[M][2];
 
 		//維護每首歌的中繼點
 		for (int i = 0; i < M; i++){
-			int from_song, from_time, to_song, to_time;
-			cin>>from_song>>from_time>>to_song>>to_time;
+			int s1, t1, s2, t2;
+			cin>>s1>>t1>>s2>>t2;
 			
-			if ((from_song == to_song) && (from_time >= to_time)){	//同一首歌自己有cycle
+			if ((s1 == s2) && (t1 >= t2)){	//同一首歌自己有cycle
 				answer[p] = -1;
-				break;
-			}
-			if (nodemap[from_song].find(from_time) == nodemap[from_song].end()) {
-				nodemap[from_song].insert(make_pair(from_time, ++nodecount));
-			}
-			if (nodemap[to_song].find(to_time) == nodemap[to_song].end()) {
-				nodemap[to_song].insert(make_pair(to_time, ++nodecount));
 			}
 			
-			jumppoint[i][0] = from_song;
-			jumppoint[i][1] = from_time;
-			jumppoint[i][2] = to_song;
-			jumppoint[i][3] = to_time;
+
+			int index = bsearch(0, nodemap[s1][0].size()-1, t1, nodemap[s1][0]);
+			if (index < nodemap[s1][0].size()){
+				if (nodemap[s1][0][index] != t1){
+					nodemap[s1][0].insert(nodemap[s1][0].begin()+index, t1);
+					nodemap[s1][1].insert(nodemap[s1][1].begin()+index, ++nodecount);	
+				}
+			}
+			else {
+				nodemap[s1][0].insert(nodemap[s1][0].begin()+index, t1);
+				nodemap[s1][1].insert(nodemap[s1][1].begin()+index, ++nodecount);
+			}
+			jumppoint[i][0] = nodemap[s1][1][index];
+
+			index = bsearch(0, nodemap[s2][0].size()-1, t2, nodemap[s2][0]);
+			if (index < nodemap[s2][0].size()){
+				if (nodemap[s2][0][index] != t2){
+					nodemap[s2][0].insert(nodemap[s2][0].begin()+index, t2);
+					nodemap[s2][1].insert(nodemap[s2][1].begin()+index, ++nodecount);	
+				}
+			}
+			else {
+				nodemap[s2][0].insert(nodemap[s2][0].begin()+index, t2);
+				nodemap[s2][1].insert(nodemap[s2][1].begin()+index, ++nodecount);
+			}
+
+			jumppoint[i][1] = nodemap[s2][1][index];
 		}
 		if (answer[p] == -1) continue;
+		
 		Graph g(nodecount+1);
 		
 		//維護除了jump以外的邊
 		for (int i = 1; i <= N; i++){
-			vector<int> keys(nodemap[i].size());
-			it = nodemap[i].begin();
-			for (int j = 0; j < keys.size(); j++){
-				keys[j] = it->first;
-				it++;
-			}
-			sort(keys.begin(), keys.end());
-			for (int j = 0; j < keys.size()-1; j++){
-				g.AddEdge(nodemap[i][keys[j]], nodemap[i][keys[j+1]], keys[j+1] - keys[j]);
+			for (int j = 0; j < nodemap[i][0].size()-1; j++){
+				g.AddEdge(nodemap[i][1][j], nodemap[i][1][j+1], nodemap[i][0][j+1] - nodemap[i][0][j]);
 			}
 		}
 
 		//維護jump的邊
 		for (int i = 0; i < M; i++){
-			int from_index, to_index;
-			from_index = nodemap[jumppoint[i][0]][jumppoint[i][1]];
-			to_index = nodemap[jumppoint[i][2]][jumppoint[i][3]];
-			g.AddEdge(from_index, to_index, 1);
+			g.AddEdge(jumppoint[i][0], jumppoint[i][1], 1);
 		}
 		
-
+		//g.show();
 		//計算
-		
 		if (g.isCyclic()){
 			answer[p] = -1;
-			continue;
 		}
-		g.topologicalSort();
-		answer[p] = g.finddiameter() + 1;
-		
+		else {
+			g.topologicalSort();
+			answer[p] = g.finddiameter() + 1;
+		}
+
 	}
 	
 	for (int i = 0; i < testcase; i++)
